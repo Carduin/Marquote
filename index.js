7ec3a178899972f2c87b2f6d3b7b3ed1e53182db
Arthur Murillo <arthurmurillo9@gmail.com>
2021-01-09 11:50:06 +0100

    Refonte structure code, ajout commande pour donner préfixe si bot mentionné

--- index.js
+++ index.js
@@ -25,82 +25,90 @@ bot.on('ready', () => {
 bot.on('message', msg => {
     const args = msg.content.trim().split(' ');
     const command = args.shift().toLowerCase();
-    if (command === 'ping') {
-        msg.channel.send('pong');
-    }
-    if (command === PREFIX + 'help') {
-        msg.channel.send({
-            embed: {
-                color: 3066993,
-                title: "Aide",
-                description: "Pour obtenir une citation du dieu Marqua, écris **" + PREFIX + "m**. Pour mettre à jour les citations, écris **" + PREFIX + "fill**. ***Attention*** ! Les citations doivent commencer et se finir par des guillements !",
-                footer: {
-                    text: "© Marquote, a Discord bot made by Carduin"
-                }
-            }
-        });
-    }
-    if (command === PREFIX + "fill") {
-        quotes = [];
-        quotesChannel = bot.channels.cache.get(QUOTES_CHANNEL_ID);
-        if (quotesChannel !== undefined) {
-            getQuotesFromGivenChannelAndFlagIfNotCorrect(quotesChannel, 1000).then(messages => {
-                messages.forEach(message => {
-                    quotes.push(message);
-                });
-                fs.writeFileSync('data.json', JSON.stringify(quotes,null, 4));
-                quotesData = JSON.parse(fs.readFileSync('data.json'));
 
-                invalidQuotesFoundErrorMessage = "";
-
-                if(invalidQuotesFound) {
-                    invalidQuotesFoundErrorMessage = " **Attention**, des citations invalides ont été trouvées, et marquées avec ❌.";
-                }
-
-                msg.channel.send("Citations mises à jour (" + messages.length +") !" + invalidQuotesFoundErrorMessage)
-            });
-        }
-        else {
-            msg.channel.send("Je n'ai pas accès au salon des citations indiqué !")
-        }
-
-    }
-    if (command === PREFIX + "m") {
-        if(quotesData != undefined) {
-            randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
+    switch(command) {
+        case 'ping':
+            msg.channel.send('pong');
+            break;
+        case PREFIX + 'help' :
             msg.channel.send({
                 embed: {
-                    color: 15844367,
-                    title: "Marqua dit...",
-                    description: randomQuote,
+                    color: 3066993,
+                    title: "Aide",
+                    description: "Pour obtenir une citation du dieu Marqua, écris **" + PREFIX + "m**. Pour mettre à jour les citations, écris **" + PREFIX + "fill**. ***Attention*** ! Les citations doivent commencer et se finir par des guillements !",
                     footer: {
-                        text: "© Christophe Marquesuzaà"
+                        text: "© Marquote, a Discord bot made by Carduin"
                     }
                 }
             });
-        }
-        else {
-            msg.channel.send("Pas de fichier de quotes. Merci d'exécuter la commande **" + PREFIX + "fill**.")
-        }
+            break;
+        case PREFIX + 'fill' :
+            quotes = [];
+            quotesChannel = bot.channels.cache.get(QUOTES_CHANNEL_ID);
+            if (quotesChannel !== undefined) {
+                getQuotesFromGivenChannelAndFlagIfNotCorrect(quotesChannel, 1000).then(messages => {
+                    messages.forEach(message => {
+                        quotes.push(message);
+                    });
+                    fs.writeFileSync('data.json', JSON.stringify(quotes,null, 4));
+                    quotesData = JSON.parse(fs.readFileSync('data.json'));
+
+                    invalidQuotesFoundErrorMessage = "";
+
+                    if(invalidQuotesFound) {
+                        invalidQuotesFoundErrorMessage = " **Attention**, des citations invalides ont été trouvées, et marquées avec ❌.";
+                    }
+
+                    msg.channel.send("Citations mises à jour (" + messages.length +") !" + invalidQuotesFoundErrorMessage)
+                });
+            }
+            else {
+                msg.channel.send("Je n'ai pas accès au salon des citations indiqué !")
+            }
+            break;
+        case PREFIX + 'm' :
+            if(quotesData != undefined) {
+                randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
+                msg.channel.send({
+                    embed: {
+                        color: 15844367,
+                        title: "Marqua dit...",
+                        description: randomQuote,
+                        footer: {
+                            text: "© Christophe Marquesuzaà"
+                        }
+                    }
+                });
+            }
+            else {
+                msg.channel.send("Pas de fichier de quotes. Merci d'exécuter la commande **" + PREFIX + "fill**.")
+            }
+            break;
+        case PREFIX + 'speak' :
+            if (msg.author.id === ADMIN_ID && msg.guild !== null) { //If sender is the bot admin OR message is not in a DM
+                msg.delete();
+                msg.channel.send(args.join(' '));
+            }
+            break;
+        case '&p' :
+            listePresentiel = [
+                "<@377171004167946242>", //Arthur
+                "<@660418243718283284>", //Marina
+                "<@338248910717976577>", //Damien
+                "<@321684578240823297>", //Matthieu
+                "<@398573484931809290>", //Lea
+                "<@367636529599873024>" //Thomas
+            ];
+            if(msg.channel.id == "737389053695098932") {
+                msg.channel.send( listePresentiel.join(" ") + ", Venez ouvrir la porte ou je vous met un coup de boule rotatif à Mach 20")
+            }
+            break;
+        default:
+            break;
     }
-    if (command === PREFIX + 'speak') {
-        if (msg.author.id === ADMIN_ID && msg.guild !== null) { //If sender is the bot admin OR message is not in a DM
-            msg.delete();
-            msg.channel.send(args.join(' '));
-        }
-    }
-    if (command === "&p") {
-        listePresentiel = [
-            "<@377171004167946242>", //Arthur
-            "<@660418243718283284>", //Marina
-            "<@338248910717976577>", //Damien
-            "<@321684578240823297>", //Matthieu
-            "<@398573484931809290>", //Lea
-            "<@367636529599873024>" //Thomas
-        ];
-        if(msg.channel.id == "737389053695098932") {
-            msg.channel.send( listePresentiel.join(" ") + ", Venez ouvrir la porte ou je vous met un coup de boule rotatif à Mach 20")
-        }
+
+    if (msg.mentions.has(bot.user)) {
+        msg.channel.send('Mon préfixe est : **' + PREFIX + "**");
     }
 });
 
