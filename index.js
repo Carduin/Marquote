5308290cba129422d87b1e6b55db9d30d9552fba
Arthur Murillo <arthurmurillo9@gmail.com>
2021-01-09 17:37:07 +0100

    Ajout d'une commande d'autocomplétion, résolution de conflits

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const TOKEN = process.env.TOKEN;
 const PREFIX = process.env.PREFIX;
 const ADMIN_ID = process.env.ADMIN_ID;
 const QUOTES_CHANNEL_ID = process.env.QUOTES_CHANNEL_ID;
-const KEYWORDS_REACTION_CHANNEL_ID = process.env.KEYWORDS_REACTION_CHANNEL_ID;
 var invalidQuotesFound;
 
 bot.login(TOKEN);
@@ -22,7 +21,6 @@ try {
 
 bot.on('ready', () => {
     console.info("\n\n\"Faire de l'agilité, ce n'est pas faire de l'arrache !\"\n\n" + bot.user.tag + "\n\n");
-    var index = 0;
     bot.user.setActivity(PREFIX + 'help', {type: 'PLAYING'})
 });
 
@@ -30,35 +28,46 @@ bot.on('message', msg => {
     const args = msg.content.trim().split(' ');
     const command = args.shift().toLowerCase();
 
+    var hasArguments = args.length > 0;
     var authorIsNotSelf = msg.author.id !== bot.user.id;
     var authorIsAdmin = msg.author.id === ADMIN_ID;
     var botIsMentionned = msg.mentions.has(bot.user);
     var messageIsNotDM = msg.guild !== null;
-    var messageIsFromKeyWordsReactionChannel = msg.channel.id === KEYWORDS_REACTION_CHANNEL_ID;
     var keywordsDataExists = keywordsData !== undefined;
     var quotesDataExists = quotesData !== undefined;
     var quotesChannelIsAccessible = bot.channels.cache.get(QUOTES_CHANNEL_ID) !== undefined;
-    var keywordsChannelIsAccessible = bot.channels.cache.get(KEYWORDS_REACTION_CHANNEL_ID) !== undefined;
-    var hasNotAlreadyAnsweredKeyword = true;
+    var hasAnswered = false;
 
-
+    //Classic commands
     switch(command) {
         case 'ping':
             msg.channel.send('pong');
+            hasAnswered = true;
             break;
         case PREFIX + 'help' :
             msg.channel.send({
                 embed: {
                     color: 3066993,
                     title: "Aide",
-                    description: "Pour obtenir une citation du dieu Marqua, écris **" + PREFIX + "m**. Pour mettre à jour les citations, écris **" + PREFIX + "fill**. ***Attention*** ! Les citations doivent commencer et se finir par des guillements !",
+                    description: "Liste des commandes :",
+                    fields: [
+                        {
+                            name: 'Obtenir une citation',
+                            value: "Pour obtenir une citation du dieu Marqua, écris **" + PREFIX + "m**. Pour mettre à jour les citations, écris **" + PREFIX + "f**. ***Attention*** ! Les citations doivent commencer et se finir par des guillements !"
+                        },
+                        {
+                            name: 'Compléter une citation',
+                            value: "Pour que le bot termine une citation, écris **" + PREFIX + "c** suivi du début de la citation de ton choix."
+                        }
+                    ],
                     footer: {
                         text: "© Marquote, a Discord bot made by Carduin"
                     }
                 }
             });
+            hasAnswered = true;
             break;
-        case PREFIX + 'fill' :
+        case PREFIX + 'f' :
             quotes = [];
             quotesChannel = bot.channels.cache.get(QUOTES_CHANNEL_ID);
             if (quotesChannelIsAccessible) {
@@ -79,8 +88,8 @@ bot.on('message', msg => {
 
                     var sentence = "";
                     quotesData.forEach(quote =>  {
-                        //Nettoyage des citations : suppression des guillemets en début / fin, suppression d'espaces inutiles et suppression d'émojis
-                        quote = quote.slice(1,-1).trim().replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')
+                        //Nettoyage des citations : suppression d'espaces inutiles et suppression d'émojis
+                        quote = quote.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')
                         //Suppression des tirets
                         quote = quote.replace(/-/g, '').trim();
                         //Suppression des étoiles
@@ -104,6 +113,7 @@ bot.on('message', msg => {
             else {
                 msg.channel.send("Je n'ai pas accès au salon des citations indiqué !")
             }
+            hasAnswered = true;
             break;
         case PREFIX + 'm' :
             if(quotesDataExists) {
@@ -112,22 +122,47 @@ bot.on('message', msg => {
                     embed: {
                         color: 15844367,
                         title: "Marqua dit...",
-                        description: randomQuote,
-                        footer: {
-                            text: "© Christophe Marquesuzaà"
-                        }
+                        description: randomQuote
                     }
                 });
             }
             else {
                 msg.channel.send("Pas de fichier de quotes. Merci d'exécuter la commande **" + PREFIX + "fill**.")
             }
+            hasAnswered = true;
             break;
-        case PREFIX + 'speak' :
+        case PREFIX + 'c' :
+            //Auto complete
+            if(authorIsNotSelf && quotesDataExists) {
+                if(hasArguments) {
+                    var NoQuoteFound = true;
+                    var currentQuote = 0;
+                    var substring = args.join(' ');
+                    while (NoQuoteFound && currentQuote < quotesData.length) {
+                        if (quotesData[currentQuote].startsWith(substring) ) {
+                            msg.channel.send("..." + quotesData[currentQuote ].replace(substring, ''));
+                            NoQuoteFound = false;
+                        }
+                        else {
+                            currentQuote++;
+                        }
+                    }
+                    if(NoQuoteFound) {
+                        msg.channel.send("Alors la je suis sans mots...Je n'ai rien trouvé à compléter");
+                    }
+                }
+                else {
+                    msg.channel.send("Merci de saisir des mots pour compléter la citation");
+                }
+                hasAnswered = true;
+            }
+            break;
+        case PREFIX + 's' :
             if (authorIsAdmin && messageIsNotDM) { //If sender is the bot admin OR message is not in a DM
                 msg.delete();
                 msg.channel.send(args.join(' '));
             }
+            hasAnswered = true;
             break;
         case '&p' :
             listePresentiel = [
@@ -140,29 +175,38 @@ bot.on('message', msg => {
             ];
             if(msg.channel.id == "737389053695098932") {
                 msg.channel.send( listePresentiel.join(" ") + ", Venez ouvrir la porte ou je vous met un coup de boule rotatif à Mach 20")
+                hasAnswered = true;
             }
             break;
         default:
             break;
     }
 
-    if(authorIsNotSelf && keywordsDataExists) {
-        keywordsData.forEach(keyword => {
-            if (msg.content.includes(keyword) && hasNotAlreadyAnsweredKeyword) {
-                if(quotesData != undefined) {
-                    randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
-                    msg.channel.send(randomQuote.slice(1,-1).trim());
-                    hasNotAlreadyAnsweredKeyword = false;
-                }
+    //Keywords reaction
+    if(authorIsNotSelf && keywordsDataExists && quotesDataExists && !hasAnswered) {
+        var keywordNotFound = true;
+        var currentKeyword = 0;
+        while (keywordNotFound && currentKeyword < keywordsData.length) {
+            if(msg.content.includes(keywordsData[currentKeyword])) {
+                randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
+                msg.channel.send(randomQuote);
+                hasAnswered = true;
+                keywordNotFound = false;
             }
-        })
+            else {
+                currentKeyword ++;
+            }
+        }
     }
 
+    //Help if mentionned
     if (botIsMentionned) {
         if (msg.content === "<@!" + bot.user.id + ">" || msg.content === "<@!" + bot.user.id + "> ") {
             msg.channel.send('Mon préfixe est : **' + PREFIX + "**");
+            hasAnswered = true;
         }
     }
+
 });
 
 function removeRandomElementsFromArray(arr, newLength) {
@@ -187,7 +231,7 @@ async function getQuotesFromGivenChannelAndFlagIfNotCorrect(channel, limit = 500
         messages.forEach(message => {
             var content = message.content;
             if ((content.startsWith("\"") && content.endsWith("\"")) || (content.startsWith("«") && content.endsWith("»")) ) {
-                sentContent = message.content.replace("«", "\"").replace("»", "\"");
+                sentContent = message.content.slice(1,-1).trim();
                 messagesContent.push(sentContent);
             }
             else {
