496f63c27b67b92c888beae62834e8391b032331
Arthur Murillo <arthurmurillo9@gmail.com>
2021-01-31 15:12:53 +0100

    Première base de la refonte BD

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const https = require('https');
-const fs = require('fs');
+const mysql = require('mysql');
 const keyword_extractor = require("keyword-extractor");
 
 const bot = new Discord.Client();
@@ -9,24 +8,33 @@ const TOKEN = process.env.TOKEN;
 const PREFIX = process.env.PREFIX;
 const ADMIN_ID = process.env.ADMIN_ID;
 const QUOTES_CHANNEL_ID = process.env.QUOTES_CHANNEL_ID;
-
-var quotesData;
-var keywordsData;
+const DB_HOST= process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME= process.env.DB_NAME;
+const DB_USER= process.env.DB_USER;
+const DB_PASSWORD= process.env.DB_PASSWORD;
+let dbConnection;
 
 bot.login(TOKEN);
-loadData();
 
 bot.on('ready', () => {
     console.info("\n\n\"Faire de l'agilité, ce n'est pas faire de l'arrache !\"\n\n" + bot.user.tag + "\n\n");
     bot.user.setActivity(PREFIX + 'help', {type: 'PLAYING'})
+
+    dbConnection = mysql.createConnection({
+        host     : DB_HOST,
+        port     : DB_PORT,
+        user     : DB_USER,
+        password : DB_PASSWORD,
+        database : DB_NAME
+    });
+
+    checkDatabaseSetup();
+
+    addQuote("\"prout\"")
 });
 
 bot.on('message', msg => {
-
-    if(keywordsData === undefined || quotesData === undefined) {
-        loadData()
-    }
-
     const args = msg.content.trim().split(' ');
     const command = args.shift().toLowerCase();
 
@@ -35,15 +43,11 @@ bot.on('message', msg => {
     var authorIsAdmin = msg.author.id === ADMIN_ID;
     var botIsMentionned = msg.mentions.has(bot.user);
     var messageIsNotDM = msg.guild !== null;
-    var keywordsDataExists = keywordsData !== undefined;
-    var quotesDataExists = quotesData !== undefined;
-    var hasAnswered = false;
 
     //Classic commands
     switch(command) {
         case 'ping':
             msg.channel.send('pong');
-            hasAnswered = true;
             break;
         case PREFIX + 'help' :
             msg.channel.send({
@@ -66,100 +70,26 @@ bot.on('message', msg => {
                     }
                 }
             });
-            hasAnswered = true;
-            break;
-        case PREFIX + 'f' :
-            loadData();
-            msg.channel.send("Citations mises à jour (" + quotesData.length +") !");
-            hasAnswered = true;
-            break;
-        case PREFIX + 'm' :
-            if(quotesDataExists) {
-                randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
-                msg.channel.send({
-                    embed: {
-                        color: 15844367,
-                        title: "Marqua dit...",
-                        description: randomQuote
-                    }
-                });
-            }
-            else {
-                msg.channel.send("Pas de fichier de quotes. Merci d'exécuter la commande **" + PREFIX + "f**.")
-            }
-            hasAnswered = true;
-            break;
-        case PREFIX + 'c' :
-            //Auto complete
-            if(authorIsNotSelf && quotesDataExists) {
-                if(hasArguments) {
-                    var NoQuoteFound = true;
-                    var currentQuote = 0;
-                    var substring = args.join(' ');
-                    while (NoQuoteFound && currentQuote < quotesData.length) {
-                        currentQuoteData = quotesData[currentQuote];
-                        if (currentQuoteData.toLowerCase().startsWith(substring.toLowerCase()) ) {
-                            msg.channel.send("..." + currentQuoteData.substring(substring.length));
-                            NoQuoteFound = false;
-                        }
-                        else {
-                            currentQuote++;
-                        }
-                    }
-                    if(NoQuoteFound) {
-                        msg.channel.send("Alors la je suis sans mots...Je n'ai rien trouvé à compléter");
-                    }
-                }
-                else {
-                    msg.channel.send("Merci de saisir des mots pour compléter la citation");
-                }
-                hasAnswered = true;
-            }
             break;
         case PREFIX + 's' :
             if (authorIsAdmin && messageIsNotDM) { //If sender is the bot admin OR message is not in a DM
                 msg.delete();
                 msg.channel.send(args.join(' '));
             }
-            hasAnswered = true;
             break;
         default:
             break;
     }
-/*
-    //Keywords reaction
-    if(authorIsNotSelf && keywordsDataExists && quotesDataExists && !hasAnswered) {
-        var matchNotFound = true;
-        var currentKeywordObject = 0;
-        while (matchNotFound && currentKeywordObject < keywordsData.length) {
-            var keywordMatchNumber = 0;
-            keywordsData[currentKeywordObject].keywords.forEach(keyword => {
-                if(msg.content.toLowerCase().includes(keyword.toLowerCase())) {
-                    keywordMatchNumber++;
-                }
-            })
-            if(keywordMatchNumber >= keywordsData[currentKeywordObject].keywords.length/3) {
-                var oddsPercentage = Math.random() * 100;
-                if(oddsPercentage >= 80) { // 20% de chances
-                    msg.channel.send(keywordsData[currentKeywordObject].quote);
-                }
-                matchNotFound = false;
-            }
-             else {
-                currentKeywordObject ++;
-            }
-        }
-    }
-*/
     //Help if mentionned
     if (botIsMentionned) {
         if (msg.content === "<@!" + bot.user.id + ">" || msg.content === "<@!" + bot.user.id + "> ") {
             msg.channel.send('Mon préfixe est : **' + PREFIX + "**");
-            hasAnswered = true;
         }
     }
 
 });
+
+/////////////////////// USEFUL FUNCTIONS
 
 function removeRandomElementsFromArray(arr, newLength) {
     var a = arr.slice();
@@ -204,57 +134,111 @@ function randInt(max, min) {
     return ((min | 0) + Math.random() * (max + 1)) | 0;
 }
 
-function loadData() {
-    writeData()
-    try {
-        quotesData = JSON.parse(fs.readFileSync('quotes.json'));
-        keywordsData = JSON.parse(fs.readFileSync('keywords.json'));
-    } catch (e) {
-        quotesData = undefined;
-        keywordsData = undefined;
+/////////////////////// DATABASE FUNCTIONS
+
+function checkDatabaseSetup() {
+    dbConnection.query("CREATE TABLE IF NOT EXISTS quotes (id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255), cooldown INT(2))", function (err, result) {
+    });
+
+    dbConnection.query("CREATE TABLE IF NOT EXISTS keywords (id INT PRIMARY KEY, idQuote INT(4), text VARCHAR(255))", function (err, result) {
+    });
+}
+
+function getAllQuotes () {
+    /*
+    Appel :
+    getAllQuotes().then(rows => {
+        console.log(rows);
+    })
+     */
+    return new Promise(function (resolve, reject) {
+        dbConnection.query("SELECT * FROM quotes", function (err, result) {
+            if (err) {
+                reject(err);
+            }
+            if (result.length > 0) {
+                objectArray = [];
+                result.forEach(currentResult => {
+                    objectArray.push(new Quote(currentResult.id, currentResult.text, currentResult.cooldown))
+                })
+                resolve(objectArray);
+            }
+        })
+    })
+
+}
+
+function getQuote (id) {
+
+}
+
+function getKeyWordsByQuote (id) {
+
+}
+
+function addQuote (text) {
+    dbConnection.query("INSERT INTO quotes (`text`, `cooldown`) VALUES ('" + text + "', 5)", function (err, result) {
+    });
+}
+
+function addQuoteKeyword (text, quoteId) {
+
+}
+
+function editQuote (quoteObject) {
+
+}
+
+/////////////////////// CLASSES
+
+class Quote {
+    #id;
+    #text;
+    #cooldown;
+
+    constructor (id, quote, cooldown) {
+        this.id = id;
+        this.quote = quote;
+        this.cooldown = cooldown;
+    }
+
+    getId() {
+        return this.#id;
+    }
+
+    getText() {
+        return this.#text;
+    }
+
+    getCooldown() {
+        return this.#cooldown;
+    }
+
+    setCooldown(cooldown) {
+        this.#cooldown = cooldown;
     }
 }
 
-function writeData() {
-    let quotes = [];
-    let quotesChannel = bot.channels.cache.get(QUOTES_CHANNEL_ID);
-    let quotesChannelIsAccessible = bot.channels.cache.get(QUOTES_CHANNEL_ID) !== undefined
-    if (quotesChannelIsAccessible) {
-        getQuotesFromGivenChannelAndFlagIfNotCorrect(quotesChannel, 1000).then(messages => {
-            messages.forEach(message => {
-                quotes.push(message);
-            });
-            fs.writeFileSync('quotes.json', JSON.stringify(quotes,null, 4));
-            quotesData = JSON.parse(fs.readFileSync('quotes.json'));
+class Keyword {
+    #id;
+    #idQuote;
+    #text;
 
-            var keyWordsArray = [];
-            quotesData.forEach(quote =>  {
-                originalQuote = quote;
-                //Nettoyage des citations : suppression d'espaces inutiles et suppression d'émojis
-                quote = quote.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')
-                //Suppression des tirets
-                quote = quote.replace(/-/g, '').trim();
-                //Suppression des étoiles
-                quote = quote.replace(/\*/g, '').trim();
+    constructor (id, idQuote, text) {
+        this.id = id;
+        this.idQuote = idQuote;
+        this.text = text;
+    }
 
-                var extraction_result = keyword_extractor.extract(quote,{
-                    language:"french",
-                    remove_digits: true,
-                    return_changed_case:false,
-                    remove_duplicates: true
-                });
-                extraction_result = removeRandomElementsFromArray(extraction_result, extraction_result.length);
+    getId() {
+        return this.#id;
+    }
 
-                if(extraction_result.length > 0 ) {
-                    keyWordsArray.push({
-                        keywords: extraction_result,
-                        quote: originalQuote
-                    })
-                }
-            })
+    getText() {
+        return this.#text;
+    }
 
-            fs.writeFileSync('keywords.json', JSON.stringify(keyWordsArray,null, 4));
-            keywordsData = JSON.parse(fs.readFileSync('keywords.json'));
-        });
+    getQuoteId() {
+        return this.#idQuote;
     }
 }
